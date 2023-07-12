08a28d989b92c9a8e80e0d0251cfac5758478a6b
Gagan2699 <86026670+Gagan2699@users.noreply.github.com>
2023-07-11 23:45:01 -0500

    Create test.go

---
+++ test.go
@@ -0,0 +1,15 @@
+package actions
+
+import (
+	"fmt"
+	"runtime"
+
+	"rsc.io/quote"
+)
+
+func Demo() {
+	fmt.Printf("Go version: %s\n", runtime.Version())
+	fmt.Printf("GOOS: %s\n", runtime.GOOS)
+
+	fmt.Println(quote.Go())
+}
